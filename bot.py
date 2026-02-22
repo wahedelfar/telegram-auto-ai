bd2263b34a3fd7c4c66952ab22df838c0b8b45bb
wahedelfar <damietta4@gmail.com>
2026-02-22 05:24:26 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = "8017128234:AAHiTswqIn7dYfsrpHWz8vw_V_UgE71TrSE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("البوت شغال 🔥")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
